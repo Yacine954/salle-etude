e87a6bbc6f9a2a063e5ad7331fb43380d4422f43
Yacine Guettari <yacineguett@gmail.com>
2026-09-04 20:16:58 +0200

    Mise a jour du 04/09/2026 a 20:16

--- docs/sw.js
+++ docs/sw.js
@@ -3,7 +3,7 @@
    ce qui déclenche la mise à jour chez les visiteurs. */
 "use strict";
 
-var VERSION = "5de5d8048910";
+var VERSION = "cace76c08860";
 var CACHE = "salle-etude-" + VERSION;
 var POLICES = "salle-etude-polices";
 var FICHIERS = ["index.html","manifest.webmanifest","icone-192.png","icone-512.png","icone-maskable-512.png","icone-apple-180.png"];
